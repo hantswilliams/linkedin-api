5d090f9e1ab75463fdd455100ca79c3d2ccc99d8
Hants Williams <hantsawilliams@gmail.com>
2023-05-05 11:59:57 -0500

    Update cookie_repository.py

--- linkedin_api/cookie_repository.py
+++ linkedin_api/cookie_repository.py
@@ -1,5 +1,4 @@
 import io
-import os
 import pickle
 import time
 import linkedin_api.settings as settings
@@ -7,7 +6,6 @@ import linkedin_api.settings as settings
 
 class Error(Exception):
     """Base class for other exceptions"""
-
     pass
 
 
@@ -18,15 +16,12 @@ class LinkedinSessionExpired(Error):
 class CookieRepository(object):
     """
     Class to act as a repository for the cookies.
-
-    TODO: refactor to use http.cookiejar.FileCookieJar
     """
 
     def __init__(self):
         self.cookies = {}
 
     def save(self, cookies, username):
-        self._ensure_cookies_dir()
         with io.BytesIO() as f:
             pickle.dump(cookies, f)
             f.seek(0)
@@ -42,25 +37,6 @@ class CookieRepository(object):
             return cookies
         return None
 
-    def _ensure_cookies_dir(self):
-        if not os.path.exists(self.cookies_dir):
-            os.makedirs(self.cookies_dir)
-
-    def _get_cookies_filepath(self, username):
-        """
-        Return the absolute path of the cookiejar for a given username
-        """
-        return "{}{}.jr".format(self.cookies_dir, username)
-
-    def _load_cookies_from_cache(self, username):
-        cookiejar_filepath = self._get_cookies_filepath(username)
-        try:
-            with open(cookiejar_filepath, "rb") as f:
-                cookies = pickle.load(f)
-                return cookies
-        except FileNotFoundError:
-            return None
-
     @staticmethod
     def _is_token_still_valid(cookiejar):
         _now = time.time()
@@ -69,5 +45,4 @@ class CookieRepository(object):
                 if cookie.expires and cookie.expires > _now:
                     return True
                 break
-
         return False
